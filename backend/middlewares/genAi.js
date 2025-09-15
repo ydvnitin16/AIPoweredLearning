071489bfab5662499d663be45d980047fee48333
Nitin Yadav <ydvnitin16@gmail.com>
2025-09-15 07:50:57 +0530

    gemini 2.5-flash

--- backend/middlewares/genAi.js
+++ backend/middlewares/genAi.js
@@ -12,7 +12,7 @@ const generateTopicExp = async (req, res, next) => {
     const prompt = buildPrompt(userConfig);
 
     try {
-        const model = GenAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+        const model = GenAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
         console.log('genAi...');
         const result = await model.generateContent(prompt);
         const output = result.response.text();
